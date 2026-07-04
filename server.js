dead66083be7b49d659e320d586d540265d8cd8d
silvabolaji08-svg <silvabolaji08@gmail.com>
2026-07-04 21:50:10 +0100

    remove socket.io for vercel deployment

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ app.use(cors({
 }))
 app.use(express.json())
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({})
+  }
+  next()
+})
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ MongoDB error:', err))
